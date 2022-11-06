537832270b8793822ef4341e5259529aba18e630
789Roman789 <roma11102004@yandex.ru>
2022-11-06 23:19:00 +0300

    Minor changes in game.py

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from math import floor
 import pygame
 from pygame.draw import *
 from random import randint, uniform
@@ -5,7 +6,7 @@ import numpy as np
 pygame.init()
 
 FPS = 30
-screen = pygame.display.set_mode((1200, 900))
+screen = pygame.display.set_mode((1000, 600))
 
 RED = (255, 100, 0)
 BLUE = (0, 0, 255)
@@ -17,21 +18,21 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 def getVel():
-    vx = randint(-50, 50)
-    vy = randint(-50, 50)
+    vx = randint(-100, 100)
+    vy = randint(-100, 100)
     
     return [vx, vy]
 
 def getRandom():
-    x0 = randint(100, 1100)
-    y0 = randint(100, 900)
-    size = randint(20, 100)
+    x0 = randint(100, 900)
+    y0 = randint(100, 500)
+    size = randint(10, 75)
     color = randint(0, 5)
     return (x0, y0, size, color)
 
 def drawAngry(x0, y0, size, c):
     color = COLORS[c]
-    circle(screen, color, (x0, y0), size)
+    circle(screen, color, (x0, y0), size, 0)
     rect(screen, (0, 0, 0), (x0 - size *0.5, y0 + size *0.5, size, size * 0.2))
     circle(screen, (240, 0, 0), (x0 - size * 0.4, y0 - size*0.25), size * 0.2)
     circle(screen, (0, 0, 0), (x0 - size * 0.4, y0 - size*0.26), size * 0.09)
@@ -54,11 +55,13 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
-
+points = 0
 balls = []
 poses = []
 vels = []
 lives = []
+
+spt = 0
 
 while not finished:
     clock.tick(FPS)
@@ -66,14 +69,33 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            print('Click!')
-    
-    (x, y, s, c) = getRandom()        
-    balls.append((s, c))
-    liv = uniform(1.5, 5)
-    poses.append([x,y,0,liv])
-    vels.append(getVel())
-    lives.append(liv)
+            (x, y) = pygame.mouse.get_pos()
+            rng = range(len(balls)-1, 0, -1)
+            for i in rng:
+                (x0, y0, s,t) = poses[i]
+                if(s != floor(s)):
+                    continue
+                dx = x0 - x
+                dy = y0 - y
+                ds = dx*dx + dy*dy
+                if(ds <= s*s):
+                    points+=1
+                    print(points, s*s)
+                    del lives[i]
+                    del balls[i]
+                    del poses[i]
+                    del vels[i]
+                    break
+            
+    spt -= 1/FPS        
+    if(spt < 0):
+        (x, y, s, c) = getRandom()        
+        balls.append((s, c))
+        liv = uniform(1.5, 5)
+        poses.append([x,y,0,liv])
+        vels.append(getVel())
+        lives.append(liv)
+        spt = 1
     
     rng = range(len(balls)-1, 0, -1)
     
@@ -96,6 +118,14 @@ while not finished:
         poses[i][0] += vels[i][0] / FPS
         poses[i][1] += vels[i][1] / FPS
         (x0, y0, s, lt) = poses[i]
+        if(x0 + s > screen.get_size()[0]):
+            vels[i][0] = -abs(vels[i][0])
+        elif(x0 - s < 0):
+            vels[i][0] = abs(vels[i][0])
+        elif(y0 + s > screen.get_size()[1]):
+            vels[i][1] = -abs(vels[i][1])
+        elif(y0 - s < 0):
+            vels[i][1] = abs(vels[i][1])
         drawAngry(x0, y0, s, color)
     
     pygame.display.update()
